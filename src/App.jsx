21a8ce38403dd4113c177f4f1e4a3b0b368727c7
Praveen Kumar Upadhyay <praveenupadhyay997@gmail.com>
2024-11-16 20:51:21 +0530

    [T2] Added the complete code with the shimmer effect.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,63 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
+import Shimmer from "./component/Shimmer";
+
+const MemeCard = ({ data }) => {
+  const memeData = data || {};
+
+  return (
+    <div
+      className="bg-slate-300 border-2 rounded shadow-2xl
+       border-lime-200 w-[200px] p-5 m-5 flex flex-col justify-center hover:cursor-pointer"
+      onClick={() => {
+        window.open(memeData?.postLink);
+      }}
+    >
+      {memeData.preview && memeData.preview.length > 0 ? (
+        <img
+          src={memeData.url}
+          onError={(e) => {
+            e.target.src = memeData.preview[1] || "fallback-image-url";
+          }}
+          alt={memeData.title || "Meme Image"}
+        />
+      ) : (
+        <p>No image available</p>
+      )}
+      <p>{memeData.title || "Untitled"}</p>
+    </div>
+  );
+};
 
 function App() {
-  return <></>;
+  const [memeResourceData, setMemeResourceData] = useState([]);
+
+  useEffect(() => {
+    getMemeResources();
+  }, []);
+
+  const getMemeResources = async () => {
+    try {
+      const memeData = await fetch("https://meme-api.com/gimme/20");
+      const { memes } = await memeData.json();
+      console.log(memes);
+
+      if (memes) setMemeResourceData(memes);
+    } catch (error) {
+      console.error("Failed to fetch meme data:", error);
+    }
+  };
+
+  return !memeResourceData.length ? (
+    <Shimmer />
+  ) : (
+    <div className="flex flex-wrap">
+      {memeResourceData.map((memeData, index) => (
+        <MemeCard key={index} data={memeData} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/Shimmer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="flex flex-wrap gap-5">
+      {Array(20)
+        .fill()
+        .map((_, index) => (
+          <div
+            key={index}
+            className="relative w-[200px] h-[100px] bg-slate-300 rounded-lg overflow-hidden"
+          >
+            <div className="absolute inset-0 animate-shimmer bg-gradient-to-r from-transparent via-white/40 to-transparent"></div>
+            <div className="absolute inset-0 flex items-center justify-center">
+              Loading...
+            </div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Shimmer;
